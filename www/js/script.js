2116c6cb0bc549d5f062886b44bae9fc28cdb14d
Pedro <pedro.goulartm@gmail.com>
2018-09-26 19:50:47 -0300

    Primeiro Upload
    Upload para teste da função vibrate

---
+++ www/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('botao').addEventListener('click', clickHandler);
+    main();
+});
+
+function clickHandler(element) {
+    navigator.vibrate(200);
+}
+
+function main(){
+    
+}
