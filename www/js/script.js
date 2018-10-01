7d3e5a0d349d4bd50208a3c15e712368b4a93698
Pedro <pedro.goulartm@gmail.com>
2018-10-01 18:44:15 -0300

    Nova Interface

--- www/js/script.js
+++ www/js/script.js
@@ -4,9 +4,34 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function clickHandler(element) {
-    navigator.vibrate(200);
+    vibrate;
+    alert(sorteaNumero());
+    alert(app.contador);
+    app.contador ++;
+    addValor();
 }
 
 function main(){
-    
+    app.numeroSorteado = sorteaNumero();
+    app.contador = 0;
+    app.valoresInfo = '';
+}
+
+function vibrate(){
+    navigator.vibrate(1000);
+}
+
+function addValor(){
+    var x = document.getElementById('entrada').value;
+    if (app.valoresInfo != ''){
+        app.valoresInfo += ', ';
+    }
+    app.valoresInfo += x;
+    alert(app.valoresInfo);
+}
+
+function comparaValor(v)
+
+function sorteaNumero(){
+    return Math.floor(Math.random()*10000)+1;
 }
