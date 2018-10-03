6cd02d9747b68afb39e05ca157909d060467f814
Pedro <pedro.goulartm@gmail.com>
2018-10-03 18:40:52 -0300

    commit 4

--- www/js/script.js
+++ www/js/script.js
@@ -1,20 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('botao').addEventListener('click', clickHandler);
+    document.getElementById('reinicia').addEventListener('click', clickHola);
     main();
 });
 
 function clickHandler(element) {
-    vibrate;
-    alert(sorteaNumero());
-    alert(app.contador);
-    app.contador ++;
+    //obtem numero informado
+    app.valorInformado = document.getElementById('entrada').value;
+    
+    //salva numero informado para string
     addValor();
+    
+    //reduz contador
+    app.contador --;
+    
+    //vibrate();
+    //alert(sorteaNumero());
+    //alert(app.contador);
+    
+    
+    EscreveTentativas();
+    Condicao();
+    
+}
+
+function clickHola(element){
+    app.numeroSorteado = sorteaNumero();
+    app.contador = 10;
+    app.valorInformado = -1;
+    app.valoresInfo = '';
+    EscreveTentativas();
 }
 
 function main(){
     app.numeroSorteado = sorteaNumero();
-    app.contador = 0;
+    app.contador = 10;
+    app.valorInformado = -1;
     app.valoresInfo = '';
+    EscreveTentativas();
+    alert("numero sorteado"+app.numeroSorteado);
 }
 
 function vibrate(){
@@ -22,16 +46,28 @@ function vibrate(){
 }
 
 function addValor(){
-    var x = document.getElementById('entrada').value;
-    if (app.valoresInfo != ''){
-        app.valoresInfo += ', ';
-    }
+    var x = '<p>'+document.getElementById('entrada').value+'</p>';
     app.valoresInfo += x;
-    alert(app.valoresInfo);
 }
-
-function comparaValor(v)
 
 function sorteaNumero(){
     return Math.floor(Math.random()*10000)+1;
+}
+
+function EscreveTentativas(){
+    document.getElementById('tentativas').innerHTML = app.contador+' tentativas restantes';
+    
+    document.getElementById('valores').innerHTML = app.valoresInfo;
+}
+
+function Condicao(){
+    if (app.numeroSorteado > app.valorInformado){
+        alert('Numero Menor que o numero sorteado');
+    }
+    else if (app.numeroSorteado < app.valorInformado){
+        alert('Numero Maior que o numero sorteado');
+    } else{
+        alert('Acertou!');
+        vibrate();
+    }
 }
